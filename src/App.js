3a7e67e014c94b7c95700bcf7cdbffa16ff566b8
mr.navata <thaivannguyen.it@gmail.com>
2020-12-21 08:47:38 +0700

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Navigation from "./Navigation";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen! 9999</h2>
+      <h2>Start editing to see some magic happen! Ahii</h2>
+      <Navigation />
     </div>
   );
 }
---
+++ src/Navigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <div className="App">
+      <h1>Navigation</h1>
+    </div>
+  );
+}
